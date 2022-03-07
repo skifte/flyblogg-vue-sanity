7db953512d5eeb03914f780e5dabb09d9e24220a
Anders Skifte <anders@skifte.com>
2022-03-07 23:51:56 +0100

    Konfig for prosjektID

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "43mso7au", // find this at manage.sanity.io or in your sanity.json
+  projectId: process.env.VUE_APP_SANITY_ID, // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
   apiVersion: "2021-03-25"
